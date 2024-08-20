0452b72d2960806716d261c28c2f5d915cfc1fc9
Miss4Chan <despina.miseva@gmail.com>
2024-08-20 10:47:31 +0200

    Finished making the angular skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,9 +25,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
